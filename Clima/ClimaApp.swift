d5ba07221cf83e6acf018ad25ae7394f30deccdb
Fernando Zafe <fernandozafe@gmail.com>
2022-01-30 12:02:18 -0300

    Initial Commit

---
+++ Clima/ClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaApp.swift
+//  Clima
+//
+//  Created by Fernando Zafe on 30/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
